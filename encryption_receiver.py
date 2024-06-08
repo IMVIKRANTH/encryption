d681553f248eb96de8da6fcc2be7d235f972844d
Vikranth Ram <vikranthramlakkoju@gmail.com>
2024-06-08 18:45:27 +0530

    Updating the code

--- encryption_receiver.py
+++ encryption_receiver.py
@@ -1,32 +1,68 @@
 import socket
-import os 
+import tqdm
+import rsa
+import os
+from Crypto.Cipher import AES
 
-server=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-server.bind(("localhost",9090))
-server.listen()
+def createSocket():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    return server
 
-client ,address=server.accept()
+def createKeys():
+    # Generating public key and private key using rsa
+    public_key, private_key = rsa.newkeys(1024)
+    return public_key, private_key
 
-file_name=client.recv(1024).decode()
-print(file_name)
-file_size=client.recv(1024).decode()
-print(file_size)
+def send_and_receive(public_key, private_key, client):
+    #--> Here the public key is sent to the client and client will encrypt the message   using this public key
+    publicKey = public_key.save_pkcs1()
+    client.send(publicKey)
+    #--> The encrypted message is being received and by using the private key one        can decrypt the message
+    key = rsa.decrypt(client.recv(256), private_key)  # Adjusted for RSA 1024-bit
+    nonce = rsa.decrypt(client.recv(256), private_key)
+    return key, nonce
 
-file = open(file_name,"wb")
-file_byte=b""
-oper=False
+def main():
+    host=input("Enter the IP address:")
+    port=int(input("Enter port:"))
+    server = createSocket()
+    server.bind((host, port))
+    server.listen()
+    print(f"Server listening on port {port}...")
+    client, address = server.accept()
+    print(f"Connected to {address}")
 
-#progress= tqdm.tqdm(unit="B",unit_scale=True,unit_divisor=1000,total=int(file_size))
+    public_key, private_key = createKeys()
+    key, nonce = send_and_receive(public_key, private_key, client)
 
-while not oper:
-    data=client.recv(1024)
-    if file_byte[-5:]==b"{END}":
-        done= True
-    else:
-        file_byte +=data
-    #progress.update(1024)
+    cipher = AES.new(key, AES.MODE_EAX, nonce=nonce)
 
-file.write(file_byte)
-file.close()
-server.close()
-client.close()
+    file_name = client.recv(1024).decode('ascii')
+    file_size = int(client.recv(1024).decode('ascii'))
+    print(f"Receiving {file_name} of size {file_size} bytes")
+
+    progress = tqdm.tqdm(unit="B", unit_scale=True, unit_divisor=1024, total=file_size)
+    file_byte = b""
+
+    while True:
+        data = client.recv(1024)
+        if data[-5:] == b"<END>":
+            file_byte += data[:-5]
+            progress.update(len(data) - 5)
+            break
+        file_byte += data
+        progress.update(len(data))
+
+    decrypted_data = cipher.decrypt(file_byte)
+
+    # Save the received file with a different name to avoid conflicts
+    received_file_name = "received_" + file_name
+    with open(received_file_name, "wb") as file:
+        file.write(decrypted_data)
+
+    print(f"File received and decrypted successfully as {received_file_name}")
+    server.close()
+    client.close()
+
+if __name__ == "__main__":
+    main()
--- encryption_sender.py
+++ encryption_sender.py
@@ -1,19 +1,62 @@
 import os
 import socket
+import tqdm
+import rsa
+from Crypto.Cipher import AES
 
-port=9090
-client=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-client.connect(("localhost",port))
+def createSocket():
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    return client
 
-file=open("image.png","rb")
-file_size=os.path.getsize("image.png")
+def generateKey():
+    #-->Generating a random key and nonce using os
+    key = os.urandom(16)
+    nonce = os.urandom(16)
+    return key, nonce
 
-client.send("received_image.png".encode())
-client.send(str(file_size).encode())
+def send_key_and_nonce(client, key, nonce):
+    #--> sending encrypted message using the public key sent by the server 
+    #1)Receiving the public key:
+    public_key = client.recv(256)
+    publicKey = rsa.PublicKey.load_pkcs1(public_key)
+    #2)encrypting key and nonce:
+    encryptedKey = rsa.encrypt(key, publicKey)
+    encryptedNonce = rsa.encrypt(nonce, publicKey)
+    #3)sending encrypted key and nonce:
+    client.send(encryptedKey)
+    client.send(encryptedNonce)
 
-data=file.read()
-client.sendall(data)
-client.send(b"{END}")
+def main():
+    key, nonce = generateKey()
+    cipher = AES.new(key, AES.MODE_EAX, nonce=nonce)
 
-file.close()
-client.close()
+    host=input("Enter the IP address:")
+    port=int(input("Enter port:"))
+
+    client = createSocket()
+    client.connect((host, port))
+    send_key_and_nonce(client, key, nonce)
+
+    fileName = input("Enter the file name including the format: ")
+    file_size = os.path.getsize(fileName)
+
+    client.send(fileName.encode('ascii'))
+    client.send(str(file_size).encode('ascii'))
+
+    progress = tqdm.tqdm(range(file_size), f"Sending {fileName}", unit="B", unit_scale=True, unit_divisor=1024)
+
+    with open(fileName, "rb") as file:
+        while True:
+            bytes_read = file.read(1024)
+            if not bytes_read:
+                break
+            encrypted_data = cipher.encrypt(bytes_read)
+            client.sendall(encrypted_data)
+            progress.update(len(bytes_read))
+
+    client.send(b"<END>")
+    print("File sent successfully")
+    client.close()
+
+if __name__ == "__main__":
+    main()
